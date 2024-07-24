c9e8651d78e501eeca0c97fa34f3bbca97b2fef1
jlsneto <leitejoab@gmail.com>
2024-07-24 15:41:52 -0300

    release: add first release

--- pitanga/__init__.py
+++ pitanga/__init__.py
@@ -1,4 +1,6 @@
+from cereja import get_version_pep440_compliant
+
 from .database.operations import *
 
 VERSION = "0.0.1.final.0"
-__version__ = "0.0.1"
+__version__ = get_version_pep440_compliant(VERSION)
